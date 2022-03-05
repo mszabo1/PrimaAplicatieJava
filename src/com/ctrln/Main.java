395817c30e07bac9ff2d7e1031deb9e335b8df63
mszabo1 <marius.szabo2009@yahoo.com>
2022-03-05 19:30:00 +0200

    Modificare Main.java - stergere comentariu

--- src/com/ctrln/Main.java
+++ src/com/ctrln/Main.java
@@ -1,8 +1,6 @@
 package com.ctrln;
 
 public class Main {
-
-    // Java_Home
     public static void main(String[] args) {
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
