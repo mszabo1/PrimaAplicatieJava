ca6e197a08c5872dc5b055836060e09cd4ae049d
mszabo1 <marius.szabo2009@yahoo.com>
2022-03-05 13:57:28 +0200

    Modificare Main.java

--- src/com/ctrln/Main.java
+++ src/com/ctrln/Main.java
@@ -1,7 +1,7 @@
 package com.ctrln;
 
 public class Main {
-    // JAR- Java Archive
+
     // Java_Home
     public static void main(String[] args) {
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
