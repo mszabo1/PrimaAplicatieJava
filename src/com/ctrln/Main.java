e54a893b50e3a72efa83308ae7d6a3c8187d3a04
mszabo1 <marius.szabo2009@yahoo.com>
2022-03-05 12:09:51 +0200

    Adaugare fisier .gitignore

---
+++ src/com/ctrln/Main.java
@@ -0,0 +1,9 @@
+package com.ctrln;
+
+public class Main {
+    // JAR- Java Archive
+    // Java_Home
+    public static void main(String[] args) {
+        System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+    }
+}
