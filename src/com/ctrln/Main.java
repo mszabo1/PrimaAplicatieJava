1615a6c215dc6bbab917da3eeae94d77a4cfa65d
mszabo1 <marius.szabo2009@yahoo.com>
2022-03-07 19:25:57 +0200

    Adaugare comentarii pe mai multe linii

--- src/com/ctrln/Main.java
+++ src/com/ctrln/Main.java
@@ -3,9 +3,16 @@ package com.ctrln;
 public class Main {
 
     // Aceasta este o metoda main, care porneste o aplicatie Java
-    public static void main(String[] args) {
-        // Inainte de comentariu nu se pune text
-        // Aici vrem sa afisam pe consola IntelliJ un mesaj ales de noi
+    /*
+    Aici este un alt exemplu
+    de comentariu
+    pe mai multe linii
+     */
+    public static void main(/* args este parametrul metodei main*/String[] args) {
+        /* Inainte de comentariu nu se pune text
+           Aici vrem sa afisam pe consola IntelliJ un mesaj ales de noi
+         textul de aici face parte din comentariul multi linie
+         */
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
