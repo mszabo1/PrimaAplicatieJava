2a4f6da2dadaad68914f660bf79cfa0b52d8abbf
mszabo1 <marius.szabo2009@yahoo.com>
2022-03-10 18:56:11 +0200

    Am adaugat JavaDoc

--- src/com/ctrln/Main.java
+++ src/com/ctrln/Main.java
@@ -2,10 +2,13 @@ package com.ctrln;
 
 public class Main {
 
-    // Aceasta este o metoda main, care porneste o aplicatie Java
+    /**
+     * Metoda Main este metoda apelata de masina virtuala Java si care porneste o aplicatie Java
+     * Putem sa transmitem argumente aditionale prin parametrul <b>args</b>
+     * @param args Acesta este parametrul cu care transmitem argumente programului nostru
+     * @author Acer
+     */
     public static void main(String[] args) {
-        // Inainte de comentariu nu se pune text
-        // Aici vrem sa afisam pe consola IntelliJ un mesaj ales de noi
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
